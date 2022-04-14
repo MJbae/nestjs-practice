c3ed047b736828cc6c1fe38827697be4e0718dd1
MJbae <bmj5208@gmail.com>
2022-04-14 20:35:08 +0900

    feat: add Board model

---
+++ src/boards/board.model.ts
@@ -0,0 +1,11 @@
+export interface Board {
+    id: string;
+    title: string;
+    discription: string;
+    status: BoardStatus;
+}
+
+export enum BoardStatus {
+    PUBLIC = 'PUBLIC',
+    PRIVATE = 'PRIVATE'
+}
