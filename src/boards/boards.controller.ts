daf3a342f66fa6f493cc198b8ff6559e414d543f
MJbae <bmj5208@gmail.com>
2022-04-14 16:59:54 +0900

    feat: add board controller

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardService: BoardsService) {}
+
+    getAllTask(){
+        this.boardService
+    }
+}
