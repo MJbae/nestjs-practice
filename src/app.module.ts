61c39bc13ecff926c85cfb293c79df37ff84bedc
MJbae <bmj5208@gmail.com>
2022-04-14 17:00:18 +0900

    feat: add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule],
 })
 export class AppModule {}
