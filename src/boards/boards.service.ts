07c2bc6ad39e5fcb20b8b5acf54f93c7a58887c1
MJbae <bmj5208@gmail.com>
2022-04-15 09:31:09 +0900

    fix: add new command and error message in Exception

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dtos/create-board.dto';
@@ -12,7 +12,13 @@ export class BoardsService {
   }
 
   findBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const board = this.boards.find((board) => board.id === id);
+
+    if (!board) {
+      throw new NotFoundException('Cannot find board with the id');
+    }
+
+    return board;
   }
 
   removeBoard(id: string) {
