cc4e1b81d37be301817c8b50ff172a025ab886eb
maxwell <maxwell@aidkr.com>
2022-04-19 09:15:54 +0900

    fix: hide server port with env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+
+const serverConfig = config.get('server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(serverConfig.port);
 }
 bootstrap();
