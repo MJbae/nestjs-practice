3b4f6020034341c9e8074a70a8164225e61739ac
maxwell <maxwell@aidkr.com>
2022-04-19 09:20:56 +0900

    fix: add process.env for adjusting prod env

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -5,11 +5,11 @@ const dbConfig = config.db;
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: dbConfig.host,
-  port: dbConfig.port,
-  username: dbConfig.username,
-  password: dbConfig.password,
-  database: dbConfig.database,
+  host: process.env.RDB_HOSTNAME || dbConfig.host,
+  port: process.env.RDB_PORT || dbConfig.port,
+  username: process.env.RDB_USERNAME || dbConfig.username,
+  password: process.env.RDB_PASSWORD || dbConfig.password,
+  database: process.env.RDB_DATABASE || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: dbConfig.synchronize,
 };
